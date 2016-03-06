3ed75ec6a25dc9ecf2b296bf874dc4bc3d081266
Federico <miras.federico@gmail.com>
2016-03-06 05:55:45 -0300

    Added usernames feature both front and backend modules. Fixed service patch code

--- spec/chat-spec.js
+++ spec/chat-spec.js
@@ -5,33 +5,43 @@ require('jasmine-sinon');
 
 describe('emitMessage', function () {
     it('should call callback function with received object', function (done) {
-        var data = {};
-        var io = {};
-        io.spy = sinon.spy();
-        service.emitMessage(data, io, io.spy);
-        expect(io.spy).toHaveBeenCalledWith('chat message', data);
+        const data = {};
+        const io = {};
+        const spy = sinon.spy();
+        service.emitMessage(data, spy);
+        expect(spy).toHaveBeenCalledWith(data);
         done();
     });
 });
 
 describe('emitHistory', function () {
     it('should emit chat history to a new socket connection', function (done) {
-        var socketId = "newSocketId"
-        var messages = [];
-        var data1 = {
-            socketId : "socketid1",
-            msg : "mensaje1"
+        const socketId = "newSocketId"
+        const messages = [];
+        const data1 = {
+            socketId: "socketid1",
+            msg: "mensaje1"
         };
-        var data2 = {
-            socketId : "socketid2",
-            msg : "mensaje2"
+        const data2 = {
+            socketId: "socketid2",
+            msg: "mensaje2"
         };
-        messages.push(data1,data2);
-        var socket = {};
-        socket.spy = sinon.spy();
-        service.emitHistory(messages, socket, socket.spy);
-        expect(socket.spy).toHaveBeenCalledWith('chat message', data1);
-        expect(socket.spy).toHaveBeenCalledWith('chat message', data2);
+        messages.push(data1, data2);
+        const spy = sinon.spy();
+        service.emitHistory(messages, spy);
+        expect(spy).toHaveBeenCalledWith(data1);
+        expect(spy).toHaveBeenCalledWith(data2);
+        done();
+    });
+});
+
+describe('addUser', function () {
+    it('should add a username to a socket', function (done) {
+        const users = {};
+        const socketId = "newSocketId"
+        const username = "John Smith"
+        service.addUser(users, socketId, username);
+        expect(users[socketId]).toBe(username);
         done();
     });
 });
