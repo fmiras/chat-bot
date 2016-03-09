de4db84c5fe6f0e4417c686c58631e6994c401eb
Federico <miras.federico@gmail.com>
2016-03-08 23:46:22 -0300

    Added bot-response.json for making more flexible answers, bot tests and implementation fixed.

--- spec/bot-spec.js
+++ spec/bot-spec.js
@@ -1,5 +1,6 @@
 'use strict';
 var bot = require('../lib/chat/bot');
+var botResponse = require('../config/bot-response');
 var sinon = require('sinon');
 require('jasmine-sinon');
 
@@ -7,25 +8,25 @@ describe('comunicate', function () {
   it('should emit a wildcard message for any request', function () {
     const msg = "Hello! What's up?";
     const response = bot.comunicate(msg);
-    expect(response).toBe("Sorry i don't know how to answer to that... not yet.");
+    expect(response).toBe(botResponse.wildcard);
   });
 
   it('should say i dont know your name', function () {
     const msg = 'What is my name?';
     const response = bot.comunicate(msg);
-    expect(response).toBe("I don't know your name.");
+    expect(response).toBe(botResponse.naming.error);
   });
 
   it('should save a name and say hello', function () {
     const msg = 'Hello! My name is John smith';
     const response = bot.comunicate(msg);
-    expect(response).toBe('Hello John Smith, nice to meet you.');
+    expect(response).toBe(botResponse.greeting.replace('{:name}', 'John Smith'));
   });
 
   it('should say who you are', function () {
     const msg = 'What is my name?';
     const response = bot.comunicate(msg);
-    expect(response).toBe('Your name is John Smith.');
+    expect(response).toBe(botResponse.naming.success.replace('{:name}', 'John Smith'));
   });
 
   it('should convert the amount of pesos that the user indicates to pesos', function () {
@@ -42,12 +43,15 @@ describe('comunicate', function () {
     spyOn(bot.currencyUtil, 'getCurrencyDataJson').and.returnValue(json);
     const msg = 'Man, convert 76 pesos to dollars';
     const response = bot.comunicate(msg);
-    expect(response).toBe('76 pesos are 5.06 dollars.');
+    expect(response)
+        .toBe(botResponse.currencyConversion.success
+            .replace('{:pesos}', '76')
+            .replace('{:dollars}', '5.06'));
   });
 
   it('should not convert and say that the word is not a number', function () {
     const msg = 'Convert as2g pesos to dollars';
     const response = bot.comunicate(msg);
-    expect(response).toBe("as2g is not a number. I can't convert it to dollars.");
+    expect(response).toBe(botResponse.currencyConversion.error.replace('{:pesos}', 'as2g'));
   });
 });
